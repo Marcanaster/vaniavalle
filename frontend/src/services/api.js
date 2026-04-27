7468790c679020a4a46c750c1ec794a92f5e59b9
Marcos Almeida <Marcanaster@gmail.com>
2026-04-27 14:38:13 -0300

    dominio

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://177.7.37.184:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://academiavaniavalle.com.br:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
