5469dea1e6d2d63ebae831226123c5286b3ffb45
Marcos Almeida <Marcanaster@gmail.com>
2026-04-29 15:42:12 -0300

    bolsa desconto

---
+++ backend/DanceAcademy.Infrastructure/Migrations/20260429184134_AddDescontoBolsaToAluno.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DanceAcademy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescontoBolsaToAluno : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DescontoBolsa",
+                table: "Alunos",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescontoBolsa",
+                table: "Alunos");
+        }
+    }
+}
