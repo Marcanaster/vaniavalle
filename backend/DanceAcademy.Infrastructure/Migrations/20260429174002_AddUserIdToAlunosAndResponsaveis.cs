089fa6fc17bb0dfe044d6df95191ea904039f335
Marcos Almeida <Marcanaster@gmail.com>
2026-04-29 14:40:59 -0300

    ad migration aluno

---
+++ backend/DanceAcademy.Infrastructure/Migrations/20260429174002_AddUserIdToAlunosAndResponsaveis.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DanceAcademy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToAlunosAndResponsaveis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Responsaveis",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Alunos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Responsaveis");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Alunos");
+        }
+    }
+}
